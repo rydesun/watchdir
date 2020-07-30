7185135614d7afa987beb92ddda43ee528718703
rydesun <rydesun@gmail.com>
2020-07-30 14:44:43 +0800

    refactor(inotify)

--- src/inotify.rs
+++ src/inotify.rs
@@ -48,28 +48,36 @@ impl Watcher {
         let mut p = 0;
         while p < total {
             let raw = &buffer[p..];
-
-            let mut raw_wd = [0; 4];
-            let mut raw_len = [0; 4];
-            raw_wd.copy_from_slice(&raw[..4]);
-            raw_len.copy_from_slice(&raw[12..16]);
-            let wd = unsafe { transmute::<[u8; 4], i32>(raw_wd) };
-            let len = unsafe { transmute::<[u8; 4], u32>(raw_len) };
-
-            let raw_path =
-                String::from_utf8(raw[16..(16 + len as usize)].to_vec()).expect("invalid text");
-            let path = raw_path.trim_matches(char::from(0)).to_string();
-            let full_path = self.get_full_path(wd, path);
+            let raw_event = self.get_raw_event(raw);
+            let full_path = self.get_full_path(raw_event.wd, raw_event.path);
             events.push(Event { path: full_path });
 
-            p += 16 + len as usize;
+            p += 16 + raw_event.len as usize;
         }
         events
+    }
+    fn get_raw_event(&self, raw: &[u8]) -> RawEvent {
+        let mut raw_wd = [0; 4];
+        let mut raw_len = [0; 4];
+        raw_wd.copy_from_slice(&raw[..4]);
+        raw_len.copy_from_slice(&raw[12..16]);
+        let wd = unsafe { transmute::<[u8; 4], i32>(raw_wd) };
+        let len = unsafe { transmute::<[u8; 4], u32>(raw_len) };
+        let raw_path =
+            String::from_utf8(raw[16..(16 + len as usize)].to_vec()).expect("invalid text");
+        let path = raw_path.trim_matches(char::from(0)).to_string();
+        RawEvent { wd, len, path }
     }
     fn get_full_path(&self, wd: i32, path: String) -> PathBuf {
         let dir = self.wds[&wd].clone();
         Path::new(&dir).join(path)
     }
+}
+
+struct RawEvent {
+    wd: i32,
+    len: u32,
+    path: String,
 }
 
 pub struct Event {
