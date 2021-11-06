5db83491b5aa8b616ef625f795f8036944f7375a
rydesun <rydesun@gmail.com>
2021-11-06 15:03:50 +0800

    test(bench): compare to inotifywait

--- benches/benchmark.rs
+++ benches/benchmark.rs
@@ -1,4 +1,9 @@
-use std::{fs, path::PathBuf};
+use std::{
+    fs,
+    io::{BufRead, BufReader},
+    path::PathBuf,
+    process::Stdio,
+};
 
 use criterion::{criterion_group, criterion_main, BenchmarkId, Criterion};
 use rand::{distributions::Alphanumeric, thread_rng, Rng};
@@ -57,6 +62,167 @@ pub fn bench_move_dir_with_subdirs(c: &mut Criterion) {
     group.finish()
 }
 
+pub fn bench_program_init(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Program init");
+
+    for i in (0..=100).step_by(20) {
+        let top_dir_with_subdirs = tempfile::tempdir().unwrap();
+        let mut sub_dirs = PathBuf::new();
+        for _ in 0..i {
+            sub_dirs.push(random_string(5));
+        }
+        let deepest_dir = &top_dir_with_subdirs.path().join(&sub_dirs);
+        fs::create_dir_all(deepest_dir).unwrap();
+        for i in deepest_dir
+            .ancestors()
+            .take_while(|d| *d != top_dir_with_subdirs.path())
+        {
+            for _ in 0..=100 {
+                fs::File::create(i.join(random_string(5))).unwrap();
+            }
+        }
+
+        group.bench_function(BenchmarkId::new("watchdir", i), |b| {
+            b.iter(|| {
+                let mut p =
+                    std::process::Command::new(env!("CARGO_BIN_EXE_watchdir"))
+                        .arg("--hidden")
+                        .arg(top_dir_with_subdirs.path())
+                        .stderr(Stdio::piped())
+                        .spawn()
+                        .unwrap();
+                let mut stderr =
+                    BufReader::new(p.stderr.as_mut().unwrap()).lines().skip(1);
+                assert!(stderr
+                    .next()
+                    .unwrap()
+                    .unwrap()
+                    .contains("Initializing..."));
+                assert!(stderr
+                    .next()
+                    .unwrap()
+                    .unwrap()
+                    .contains("Initialized"));
+                p.kill()
+            })
+        });
+
+        group.bench_function(BenchmarkId::new("inotifywait", i), |b| {
+            b.iter(|| {
+                let mut p = std::process::Command::new("inotifywait")
+                    .arg("--monitor")
+                    .arg("--recursive")
+                    .arg(top_dir_with_subdirs.path())
+                    .stderr(Stdio::piped())
+                    .spawn()
+                    .unwrap();
+                let mut stderr =
+                    BufReader::new(p.stderr.as_mut().unwrap()).lines();
+                assert!(stderr
+                    .next()
+                    .unwrap()
+                    .unwrap()
+                    .contains("Setting up watches"));
+                assert!(stderr
+                    .next()
+                    .unwrap()
+                    .unwrap()
+                    .contains("Watches established"));
+                p.kill()
+            })
+        });
+    }
+    group.finish()
+}
+
+pub fn bench_program_watch_move_dir(c: &mut Criterion) {
+    let mut group = c.benchmark_group("Program watch move");
+
+    for i in (0..=100).step_by(20) {
+        let topdir = tempfile::tempdir().unwrap();
+        let from_tempdir = topdir.path().join(random_string(5));
+        let dest_tempdir = topdir.path().join(random_string(5));
+        let mut sub_dirs = PathBuf::new();
+        for _ in 0..i {
+            sub_dirs.push(random_string(5));
+        }
+        let deepest_dir = from_tempdir.join(&sub_dirs);
+        fs::create_dir_all(&deepest_dir).unwrap();
+        for i in deepest_dir.ancestors().take_while(|d| *d != topdir.path()) {
+            for _ in 0..=100 {
+                fs::File::create(i.join(random_string(5))).unwrap();
+            }
+        }
+
+        group.bench_function(BenchmarkId::new("watchdir", i), |b| {
+            let mut p =
+                std::process::Command::new(env!("CARGO_BIN_EXE_watchdir"))
+                    .arg("--hidden")
+                    .arg(topdir.path())
+                    .stdout(Stdio::piped())
+                    .stderr(Stdio::piped())
+                    .spawn()
+                    .unwrap();
+            let mut stdout =
+                BufReader::new(p.stdout.as_mut().unwrap()).lines();
+            let stderr = BufReader::new(p.stderr.as_mut().unwrap()).lines();
+            for line in stderr {
+                if line.unwrap().contains("Initialized") {
+                    break;
+                }
+            }
+
+            b.iter(|| {
+                fs::rename(&from_tempdir, &dest_tempdir).unwrap();
+                assert!(stdout.next().unwrap().unwrap().contains("Move "));
+                fs::rename(&dest_tempdir, &from_tempdir).unwrap();
+                assert!(stdout.next().unwrap().unwrap().contains("Move "));
+            });
+            p.kill().unwrap()
+        });
+
+        group.bench_function(BenchmarkId::new("inotifywait", i), |b| {
+            let mut p = std::process::Command::new("inotifywait")
+                .arg("--monitor")
+                .arg("--recursive")
+                .arg(topdir.path())
+                .stdout(Stdio::piped())
+                .stderr(Stdio::piped())
+                .spawn()
+                .unwrap();
+            let mut stdout =
+                BufReader::new(p.stdout.as_mut().unwrap()).lines();
+            let stderr = BufReader::new(p.stderr.as_mut().unwrap()).lines();
+            for line in stderr {
+                if line.unwrap().contains("Watches established") {
+                    break;
+                }
+            }
+
+            b.iter(|| {
+                fs::rename(&from_tempdir, &dest_tempdir).unwrap();
+                assert!(stdout
+                    .next()
+                    .unwrap()
+                    .unwrap()
+                    .contains("MOVED_FROM"));
+                assert!(stdout.next().unwrap().unwrap().contains("MOVED_TO"));
+                assert!(stdout.next().unwrap().unwrap().contains("MOVE_SELF"));
+                fs::rename(&dest_tempdir, &from_tempdir).unwrap();
+                assert!(stdout
+                    .next()
+                    .unwrap()
+                    .unwrap()
+                    .contains("MOVED_FROM"));
+                assert!(stdout.next().unwrap().unwrap().contains("MOVED_TO"));
+                assert!(stdout.next().unwrap().unwrap().contains("MOVE_SELF"));
+            });
+            p.kill().unwrap()
+        });
+    }
+    group.finish()
+}
+
 fn random_string(len: usize) -> String {
     thread_rng().sample_iter(&Alphanumeric).take(len).map(char::from).collect()
 }
@@ -65,5 +231,7 @@ criterion_group!(
     benches,
     bench_watch_dir_with_subdirs,
     bench_move_dir_with_subdirs,
+    bench_program_init,
+    bench_program_watch_move_dir,
 );
 criterion_main!(benches);
